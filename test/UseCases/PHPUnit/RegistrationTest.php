94a26bd1f0fa208b98f352734de3df8695266f0b
Matthias Noback <matthiasnoback@gmail.com>
2021-03-12 13:53:18 +0100

    Assignment 5

--- test/UseCases/PHPUnit/RegistrationTest.php
+++ test/UseCases/PHPUnit/RegistrationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Test\UseCases\PHPUnit;
 
 use DevPro\Application\Trainings\ScheduleTraining;
+use DevPro\Application\Trainings\UpcomingTraining;
 use DevPro\Application\Users\CreateOrganizer;
 use DevPro\Domain\Model\User\UserId;
 
@@ -23,17 +24,22 @@ final class RegistrationTest extends UseCaseTestCase
     public function aScheduledTrainingShowsUpInUpcomingTrainings(): void
     {
         // When the organizer schedules a new training called "Decoupling from infrastructure" for "2020-01-24 09:30"
+        $title = 'Decoupling from infrastructure';
         $this->container->application()->scheduleTraining(
             new ScheduleTraining(
                 $this->theOrganizer()->asString(),
                 'NL',
-                'Decoupling from infrastructure',
+                $title,
                 '2020-01-24 09:30'
             )
         );
 
         // Then it shows up on the list of upcoming trainings
-        $this->markTestIncomplete('TODO Assignment 5');
+        $upcomingTrainingTitles = array_map(
+            fn (UpcomingTraining $upcomingTraining) => $upcomingTraining->title(),
+            $this->container->application()->findAllUpcomingTrainings()
+        );
+        self::assertContains($title, $upcomingTrainingTitles);
     }
 
     private function theOrganizer(): UserId
